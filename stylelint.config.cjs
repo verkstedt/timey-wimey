2f99679422f39cd5b38a922308924acaca3e2f5c
Marek Augustynowicz <marek.augustynowicz@verkstedt.com>
2023-08-25 12:34:24 +0200

    css lint: Tweak name patterns

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,4 +1,11 @@
+const namePattern =
+  '^[amotu]-[a-z][a-zA-Z0-9]*(__[a-zA-Z0-9]*|)(--[a-zA-Z0-9]*|)$'
+
 module.exports = {
   extends: ['@verkstedt/eslint-config-verkstedt/stylelint-config'],
   plugins: ['stylelint-no-unsupported-browser-features'],
+  rules: {
+    'keyframes-name-pattern': namePattern,
+    'selector-class-pattern': namePattern,
+  },
 }
